65c43564cc37703ee88d0d3b8552b280f38039bf
ASJADSYED08 <hr49335@gmail.com>
2021-10-14 01:09:07 +0500

    first commit

---
+++ code.py
@@ -0,0 +1,28 @@
+print("Enter Value a")
+a=int(input())
+print("Enter Value b")
+b=int(input())
+print("ENTER OPERATOR")
+OPR=input()
+if OPR=='*':
+    if a==45 and b==3:
+        print("555")
+    else:    
+        print("mul=",a*b)
+if OPR=="+":
+    if a==56 and b==9:
+        print("77")
+    else:     
+        print("add=",a+b)
+if OPR=='/':
+    if a==56 and b==6:
+        print("4")
+    else:    
+        print("div=",a/b)
+if OPR=='-':
+    if a==56 and b==4:
+        print("4")
+    else:    
+        print("div=",a-b)        
+
+
